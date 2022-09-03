d31e798624d56f7fe2be0327a0888ec9afc87ffa
Mr. Packman <99031872+PackmanDude@users.noreply.github.com>
2022-09-04 00:56:34 +0300

    Update EvenOrOdd.c

--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
 		fprintf(stderr, "Must be at least 1 argument passed\n");
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 
-	int n = atoi(argv[1]);
-	printf("%s\n", (n&1 == 1) ? "Odd" : "Even");
-	return 0;
+	char *p;
+	errno = 0;
+	for (int i = 1; i < argc; ++i)
+	{
+		long n = strtol(argv[i], &p, 0);
+
+		if (*p != '\0')
+		{
+			fprintf(stderr, "Pass a number.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if ((n == LONG_MIN || n == LONG_MAX) && errno == ERANGE)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+
+		if ((n & 1) == 0)
+		{
+			printf("Even\n");
+		}
+
+		if ((n & 1) == 1)
+		{
+			printf("Odd\n");
+		}
+	}
+	return EXIT_SUCCESS;
 }
